6c2ab6adfb65fcd93f916389e01eb32381da9b20
Davi9898 <Danielvink1998@gmail.com>
2023-03-13 21:34:28 +0100

    Styling header
    Toevoeging van list item fetch onderdeel

--- js/script.js
+++ js/script.js
@@ -13,37 +13,63 @@ const {
    });
 console.log(repositories);
 
+
+
 const {
   data: { login },
   } = await octokit.rest.users.getAuthenticated();
   console.log("Hello, %s", login);
   
+
+
+  const owner = "Davi9898"; // Replace with your GitHub username
+  const repos = ["Kunstzoeker", "css-to-the-rescue-2223"];
+  
+  // Get the <ul> element you want to add the repositories to
   const repoList = document.querySelector("ul");
-
-// Loop through the repositories and create a list item for each one
-repositories.forEach(repo => {
-  // Create a new list item element
-  const listItem = document.createElement("li");
-
-  // Set the text content of the list item to the repository name
-  listItem.textContent = repo.name;
-
-  // Append the list item to the repository list
-  repoList.appendChild(listItem);
-});
-
-const owner = "Davi9898"; // Replace with your GitHub username
-const repos = ["Kunstzoeker", "css-to-the-rescue-2223"];
-
-for (const repo of repos) {
-  try {
-    const { data: repository } = await octokit.rest.repos.get({
-      owner,
-      repo
-    });
-    console.log(`Repository "${repository.full_name}" was found!`);
-    // Do something with the repository data, like displaying it on your webpage
-  } catch (error) {
-    console.log(`Repository "${repo}" not found!`);
+  
+  for (const repo of repos) {
+    try {
+      const { data: repository } = await octokit.rest.repos.get({
+        owner,
+        repo
+      });
+      console.log(`Repository "${repository.full_name}" was found!`);
+  
+      // Create a new <li> element for the repository
+      const repoItem = document.createElement("li");
+  
+      // Set the <li>'s text content to the repository name
+      const nameEl = document.createElement("h2");
+      nameEl.textContent = repository.name;
+      repoItem.appendChild(nameEl);
+  
+      // Set the <li>'s text content to the repository description
+      const descEl = document.createElement("p");
+      descEl.textContent = repository.description;
+      repoItem.appendChild(descEl);
+  
+      // Retrieve the number of commits you have made to the repository
+      const { data: commits } = await octokit.rest.repos.listCommits({
+        owner,
+        repo,
+        author: owner
+      });
+      console.log(`You have made ${commits.length} commits to the repository.`);
+  
+      // Display the number of commits in the <li>
+      const commitsEl = document.createElement("p");
+      commitsEl.textContent = `You have made ${commits.length} commits to this repository.`;
+      repoItem.appendChild(commitsEl);
+  
+      // Append the <li> to the repository list
+      repoList.appendChild(repoItem);
+  
+    } catch (error) {
+      console.log(`Repository "${repo}" not found!`);
+    }
   }
-}
+
+
+
+
